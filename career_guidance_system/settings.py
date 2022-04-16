045a8eae1b09bc574657a759fa969d59236a503f
noelkips <noellangat28@gmail.com>
2022-04-16 09:57:35 +0300

    url config for deployment

--- career_guidance_system/settings.py
+++ career_guidance_system/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 LOGIN_REDIRECT_URL = 'course:courses'
 LOGOUT_REDIRECT_URL = 'login'
 
--- career_guidance_system/urls.py
+++ career_guidance_system/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
-    path('', include('courses.urls')),
+    path('', include('users.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('courses', include('courses.urls')),
     path('donate/', include('mpesa.urls')),
 
 ]
