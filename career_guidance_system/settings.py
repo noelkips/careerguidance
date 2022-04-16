58a7997f6d158ac96900e6df181ebd163513c9d4
noelkips <noellangat28@gmail.com>
2022-04-16 09:30:33 +0300

    Update settings.py

--- career_guidance_system/settings.py
+++ career_guidance_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k^$3hg2uu209g^9kkst6utqv+1fab7#mrd*29oqcq41)s%pogl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['noel-22.herokuapp.com','127.0.0.1' ]
+ALLOWED_HOSTS = ['careerguidancekenya.herokuapp.com','127.0.0.1' ]
 # ALLOWED_HOSTS = ['5306a11ae831.ngrok.io', '127.0.0.1', 'localhost']
 
 # Application definition
