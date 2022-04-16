a486a8bfc8fb7e3676a7ae100d7e9596c1b343db
noelkips <noellangat28@gmail.com>
2022-04-16 11:11:06 +0300

    solving server 500

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personal_info/', views.personalInfo, name="personal_info"),
+    path('personalInfo/', views.personalInfo, name="personalInfo"),
     path('access/token', views.getAccessToken, name='get_mpesa_access_token'),
     path('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
     # register, confirmation, validation and callback urls
