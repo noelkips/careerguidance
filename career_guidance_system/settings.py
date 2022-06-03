5eb8de2b817365f155dea2c27109855dfddb47b3
noelkips <noelkipronolangat@gmail.com>
2022-06-03 15:01:40 +0100

    Update settings.py

--- career_guidance_system/settings.py
+++ career_guidance_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k^$3hg2uu209g^9kkst6utqv+1fab7#mrd*29oqcq41)s%pogl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['careerguidancekenya.herokuapp.com', '127.0.0.1']
 
 
 INSTALLED_APPS = [
