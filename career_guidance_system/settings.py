50ecdda5776804d646e12ade3abd1bb5f0a15df0
noelkips <noelkipronolangat@gmail.com>
2022-06-03 14:58:32 +0100

    Update settings.py

--- career_guidance_system/settings.py
+++ career_guidance_system/settings.py
@@ -24,12 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k^$3hg2uu209g^9kkst6utqv+1fab7#mrd*29oqcq41)s%pogl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['careerguidancekenya.herokuapp.com','127.0.0.1' ]
-# ALLOWED_HOSTS = ['5306a11ae831.ngrok.io', '127.0.0.1', 'localhost']
+DEBUG = False
+ALLOWED_HOSTS = [*]
 
-# Application definition
 
 INSTALLED_APPS = [
     # local
